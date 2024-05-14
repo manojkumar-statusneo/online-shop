d340e0aaf71da6d31740f28e4a15bece8b1838b2
Manoj Kumar <manoj.kumar@statusneo.com>
2024-05-14 16:45:40 +0530

    fix7

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
             source: "/api/:path*",
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "https://online-shop-ohm7mrh56-manoj-sonis-projects.vercel.app" },
+              { key: "Access-Control-Allow-Origin", value: "https://online-shop-3o9wwtpae-manoj-sonis-projects.vercel.app" },
               { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
             ]
